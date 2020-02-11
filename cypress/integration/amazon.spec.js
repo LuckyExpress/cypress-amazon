7f14104e3094925ed15073d8abce16de7b86a5c7
briandre <briandreleung@gmail.com>
2020-02-11 16:25:57 +0800

    blah blah blah

---
+++ cypress/integration/amazon.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit(
+      "https://www.amazon.co.jp/gp/product/B07YY9T1FQ/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
+    );
+  });
+
+  it("face mask", () => {
+    cy.contains("マスク ふつうサイズ");
+    cy.contains("1-Click™で今すぐ買う");
+  });
+});
